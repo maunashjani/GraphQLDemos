4696cfbad9dec48c9c8f3de9951c957a1f2ee10d
Maunash Jani <maunash@live.com>
2018-09-26 12:18:24 +0530

    ToDos Fetch Demo
    GraphQL API code fetching data of all todo list

---
+++ demo2.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var express_graphql = require('express-graphql');
+var { buildSchema } = require('graphql');
+var fetch = require('node-fetch');
+
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        message: [ToDo]
+    }
+
+    type ToDo {
+        userId: Int
+        id: Int
+        title: String
+        completed: String
+    }
+`);
+// Root resolver
+var root = {
+
+    message: () => {
+        return fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+       // .then(json => console.log(json))
+       
+   }
+};
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use('/demo2', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000, () => console.log('Server Running...'));
