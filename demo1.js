a22396f582a9e49a3b058bf0409e2529f222b497
Maunash Jani <maunash@live.com>
2018-09-26 12:17:15 +0530

    Simple Demo
    Simple Message Hello World Demo

--- demo1.js
+++ demo1.js
@@ -1,35 +1,22 @@
-//Import the packages npm
 var express = require('express');
 var express_graphql = require('express-graphql');
 var { buildSchema } = require('graphql');
-var fetch = require('node-fetch');
 
 // GraphQL schema
 var schema = buildSchema(`
     type Query {
-        message: [ToDo]
-    }
-
-    type ToDo {
-        userId: Int
-        id: Int
-        title: String
-        completed: String
+        message: String
     }
 `);
+
 // Root resolver
 var root = {
-
-    message: () => {
-        return fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => response.json())
-       // .then(json => console.log(json))
-       
-   }
+    message: () => 'Hello World! From GraphQL'
 };
+
 // Create an express server and a GraphQL endpoint
 var app = express();
-app.use('/demo', express_graphql({
+app.use('/demo1', express_graphql({
     schema: schema,
     rootValue: root,
     graphiql: true
